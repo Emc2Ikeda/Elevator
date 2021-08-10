c8dd95b07bf0c73c3f669d515b12bea681575612
Ema Ikeda <e.mc2ikeda@gmail.com>
2021-08-09 23:00:11 -0700

    Starting source code

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ElevatorConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // buttons to push
+            // enter elevator
+            Console.WriteLine("Select Floor\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10");
+            // move you up or down x number of floors
+
+            // leave elevator
+            Console.WriteLine("Goodbye");
+        }
+    }
+}
