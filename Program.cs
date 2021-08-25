d6b08f1224b5ddc3922943f0f5e5236aa4bace01
Ema Ikeda <e.mc2ikeda@gmail.com>
2021-08-24 18:33:24 -0700

    Please enter the commit message for your changes. Lines starting will be ignored, and an empty message aborts the commit.
    First commit
    Implemented elevator buttons

--- Program.cs
+++ Program.cs
@@ -6,13 +6,20 @@ namespace ElevatorConsole
     {
         static void Main(string[] args)
         {
-            // buttons to push
+            // starting floor. Assume user starts from 1F
+            int currentFloor = 1;
             // enter elevator
             Console.WriteLine("Select Floor\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10");
+            string floor = Console.ReadLine();
+            Console.WriteLine("Floor: " + floor);
+            // convert floor to int
+            int changeFloor = Convert.ToInt32(floor);
             // move you up or down x number of floors
+            currentFloor = changeFloor;
+            Console.WriteLine(currentFloor);
 
             // leave elevator
-            Console.WriteLine("Goodbye");
+            Console.WriteLine("Arrived at " + currentFloor + ".Goodbye");
         }
     }
 }
